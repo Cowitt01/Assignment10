9feb29e4fb7229e4e3e042e60296442f68311360
Cory Witt <cpwitt@mail.mccneb.edu>
2025-07-06 20:31:37 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -17,27 +17,26 @@ $(document).ready(() => {
   $('#loadingHolder').hide();
   $('#employeeList').hide();
 
-   $('#getEmployees').click((evt) => {
-   $(evt.target).parent().slideUp();
-   $('#loadingHolder').slideDown();
-
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then((employees) => {
-        for (let employee of employees) {
-            let div = document.createElement('div');
-            let a = document.createElement('a');
-            a.href = '#';
-            a.id = employee.id;
-            a.innerHTML = `${employee.first_name} ${employee.last_name}`;
-            a.addEventListener('click', onEmployeeClicked);
-            div.appendChild(a);
-            employeeList.appendChild(div);
-        }
-        $('#loadingHolder').fadeOut(400, () => {
-            $('#employeeList').fadeIn();
-        });
-    });
+  $('#getEmployees').click((evt) => {
+      $(evt.target).parent().slideUp();
+      $('#loadingHolder').slideDown();
+      fetch(baseUrl)
+      .then(response => response.json())
+          .then((employees) => {
+              for (let employee of employees) {
+                  let div = document.createElement('div');
+                  let a = document.createElement('a');
+                      a.href = '#';
+                      a.id = employee.id;
+                      a.innerHTML = `${employee.first_name} ${employee.last_name}`;
+                      a.addEventListener('click', onEmployeeClicked);
+                      div.appendChild(a);
+                      employeeList.appendChild(div);
+              }
+              $('#loadingHolder').fadeOut(400, () => {
+                  $('#employeeList').fadeIn();
+           });
+     });
 });
 
 const onEmployeeClicked = (e) => {
@@ -46,43 +45,44 @@ const onEmployeeClicked = (e) => {
     employeeInfo.innerHTML = '';
 
     fetch (`${baseUrl}\/${e.currentTarget.id}`)
-      .then(response => response.json())
-      .then((employee) => {
+    .then(response => response.json())
+    .then((employee) => {
+        let div = document.createElement('div');  
         
-        const div = document.createElement('div');
-        const img = document.createElement('img');
-       
-        const h1 = document.createElement('h1');
-        const divDepartment = document.createElement('div');
-        const divSalary = document.createElement('div');
-  
-        const divHireDate = document.createElement('div');
-       
- 
-        const fullDayName = hireDate.getFullDayName();
-        const fullMonthName = hireDate.getFullMonthName();
-        const year = hireDate.getFullYear().toString();
-        const ordinal = hireDate.getDateOrdinal();
-        const hireDateText = `${fullDayName}, ${fullMonthName}, ${ordinal}, ${year}`;
- 
-        img.src = employee.image_filename;
-        img.alt = `${employee.first_name} ${employee.last_name}`;
-        h1.innerText = `${employee.first_name} ${employee.last_name}`;
-        divDepartment.innerText = `Department: ${employee.department.name}`;
-        divSalary.innerText = `Annual Salary: ${accounting.formatMoney(employee.annual_salary)}`;
-        divHireDate.innerText = `Hire Date: ${hireDateText}`;
- 
-        div.appendChild(img);
-        div.appendChild(h1);
-        div.appendChild(divDepartment);
-        div.appendChild(divSalary);
-        div.appendChild(divHireDate);
+        let img = document.createElement('img');
+            img.src = employee.image_filename;
+            img.alt = `${employee.first_name} ${employee.last_name}`;
 
-        employeeInfo.appendChild(div);
+        let h1 = document.createElement('h1');
+            h1.innerText = `${employee.first_name} ${employee.last_name}`;
+
+        let divDepartment = document.createElement('div');
+            divDepartment.innerText = `Department: ${employee.department.name}`;
+
+        let divSalary = document.createElement('div');
+ 
+        divSalary.innerText = `Annual Salary: ${accounting.formatMoney(employee.annual_salary)}`;
+        //Default settings for accounting.js formatMoney():
+/*Currency Symbol: $
+Currency Format: %s%v (symbol before value, e.g., "$123.45")
+Decimal Separator: . (period)
+Thousands Separator: , (comma)
+Precision (Decimal Places): */
+ 
+         let divHireDate = document.createElement('div');
+            divHireDate.innerText = `Hire Date: ${employee.hire_date}`;
+
+            div.appendChild(img);
+            div.appendChild(h1);
+            div.appendChild(divDepartment);
+            div.appendChild(divSalary);
+            div.appendChild(divHireDate);
+     
+            employeeInfo.appendChild(div);
       })
       // if the api call fails the error message is displayed in the console
       .catch((e) => {
         console.log(e.message);
       });
   };
-});
+}); 
